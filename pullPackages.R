dc8b0a05106bf7172df6ec6d607b41db6d4520a7
Saulius Jokubaitis <saulius1122@gmail.com>
2017-01-20 10:21:20 +0200

    pullpackages

---
+++ pullPackages.R
@@ -0,0 +1,29 @@
+'installPackages' <- function (package1, ...)  {   
+  
+  # convert arguments to vector
+  packages <- c(package1, ...)
+  
+  # start loop to determine if each package is installed
+  for(package in packages){
+    
+    # if package is installed locally, load
+    if(package %in% rownames(installed.packages()))
+      do.call('library', list(package))
+    
+    # if package is not installed locally, download, then load
+    else {
+      install.packages(package, repos = 'http://cran.rstudio.com/')
+      do.call("library", list(package))
+    }
+  } 
+}
+
+current.list <- c('DT', 'proxy','dbscan','mlbench','randomForest','IsolationForest','autoencoder',
+                  'fclust', 'DMwR', 'HighDimOut', 'robustbase', 'SeleMix', 'httr', 'xml2', 'quantmod',
+                  'shinythemes', 'shinydashboard')
+
+
+#  ------------------------------------------------------------------------
+
+
+installPackages(current.list)
